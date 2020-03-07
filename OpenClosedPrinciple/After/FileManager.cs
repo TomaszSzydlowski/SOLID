77e12443b88a371789e7305cc91303815d459358
Tomasz Szydlowski <TSzydlowski@protonmail.com>
2020-03-07 21:44:12 +0100

    OpenClosedPrinciple

---
+++ OpenClosedPrinciple/After/FileManager.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+
+namespace OpenClosedPrinciple
+{
+    public class FileManager
+    {
+        public void SaveToFile(JurnalAfter j, string fileName, bool overwride = false)
+        {
+            if (overwride || !File.Exists(fileName))
+            {
+                File.WriteAllText(fileName, j.ToString());
+            }
+        }
+
+
+    }
+}
---
+++ OpenClosedPrinciple/Before/JurnalBefore.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OpenClosedPrinciple
+{
+    public class JurnalBefore
+    {
+        private readonly List<string> entries = new List<string>();
+        private static int count = 0;
+
+        public int AddEntry(string text)
+        {
+            entries.Add($"{++count}:{text}");
+            return count;
+        }
+
+        public void RemoveEntry(int index)
+        {
+            entries.RemoveAt(index);
+        }
+
+        public override string ToString()
+        {
+            return string.Join(Environment.NewLine, entries);
+        }
+
+        //You don't need the code, the OpenClose rule has been broken
+
+        //public void Save(string fileName)
+        //{
+        //    File.WriteAllText(fileName, ToString());
+        //}
+
+        //public static Jurnal Load(string fileName)
+        //{
+
+        //}
+
+        //public void Load(Uri uri)
+        //{
+
+        //}
+
+    }
+}
